165c3d0502096f6409f5ebd07d95ee01539c96e8
yipei <studentypz@gmail.com>
2024-04-18 11:59:36 +0200

    update - added more general useful feature and library to default template

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Button from './components/Button';
+
 function App() {
-  return <>hello</>;
+  return (
+    <>
+      <Button type="link">link</Button>
+      <Button type="button">Button</Button>
+    </>
+  );
 }
 
 export default App;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,37 @@
-import { type ComponentPropsWithoutRef } from "react";
+import { type ComponentPropsWithoutRef } from 'react';
+import { cn } from '../../utils/cn';
 
 type ButtonProps = {
-  type: "button";
-} & ComponentPropsWithoutRef<"button">;
+  type: 'button';
+  className?: string;
+} & ComponentPropsWithoutRef<'button'>;
 
 type LinkProps = {
-  type: "link";
-} & ComponentPropsWithoutRef<"a">;
+  type: 'link';
+  className?: string;
+} & ComponentPropsWithoutRef<'a'>;
 
 const Button = (props: LinkProps | ButtonProps) => {
-  const { type } = props;
-  if (type === "link") {
-    return <a className="button" {...props}></a>;
+  const { type, className } = props;
+
+  if (type === 'link') {
+    return (
+      <a
+        className={cn('text-darkprimary px-6 py-3 hover:underline', className)}
+        {...props}
+      ></a>
+    );
   }
-  if (type === "button") return <button className="button" {...props}></button>;
+  if (type === 'button')
+    return (
+      <button
+        className={cn(
+          'button bg-darkprimary hover:bg-primary px-6 py-3 text-white',
+          className,
+        )}
+        {...props}
+      ></button>
+    );
 };
 
 export default Button;
